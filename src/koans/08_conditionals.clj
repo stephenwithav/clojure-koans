da41a460cd009bfd195698a08ef3686105e13770
Steven Edwards <steven@stephenwithav.com>
2018-08-05 15:06:39 -0400

    Back.  Looking at 07 again, then back to 08.

--- src/koans/08_conditionals.clj
+++ src/koans/08_conditionals.clj
@@ -1,7 +1,7 @@
 (ns koans.08-conditionals
   (:require [koan-engine.core :refer :all]))
 
-(comment Taking a 15-minute break.)
+(comment Estoy aqui.)
 
 (defn explain-exercise-velocity [exercise-term]
   (case exercise-term
