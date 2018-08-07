db82a1f2da84b5eec710fca2381beee3d2eef3f4
Steven Edwards <steven@stephenwithav.com>
2018-08-07 12:31:17 -0400

    macros Koan, pre-recursion.

--- src/koans/24_macros.clj
+++ src/koans/24_macros.clj
@@ -9,12 +9,12 @@
 
 (defmacro infix-concise [form]
   `(~(second form) ; Note the syntax-quote (`) and unquote (~) characters!
-    __
-    __))
+    ~(first form)
+    ~(nth form 2)))
 
 (defmacro recursive-infix [form]
   (cond (not (seq? form))
-        __
+        __ ; If form's not a sequence
         (= 1 (count form))
         `(recursive-infix ~(first form))
         :else
@@ -27,13 +27,13 @@
 
 (meditations
   "Macros are like functions created at compile time"
-  (= __ (hello "Macros!"))
+  (= "Hello, Macros!" (hello "Macros!"))
 
   "I can haz infix?"
-  (= __ (infix (9 + 1)))
+  (= 10 (infix (9 + 1)))
 
   "Remember, these are nothing but code transformations"
-  (= __ (macroexpand '(infix (9 + 1))))
+  (= '(+ 9 1) (macroexpand '(infix (9 + 1))))
 
   "You can do better than that - hand crafting FTW!"
   (= '(* 10 2) (macroexpand '(infix-concise (10 * 2))))
